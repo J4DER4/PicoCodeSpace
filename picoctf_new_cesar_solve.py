742c113255582264e01697227adf997bffc97439
j4der4 <j4der4@proton.me>
2024-02-27 01:02:51 +0200

    Solved on key d

---
+++ picoctf_new_cesar_solve.py
@@ -0,0 +1,26 @@
+encoded_flag = 'dcebcmebecamcmanaedbacdaanafagapdaaoabaaafdbapdpaaapadanandcafaadbdaapdpandcac'
+
+ALPHABET = 'abcdefghijklmnop'
+LOWERCASE_OFFSET = ord("a")
+
+def shift(c, k):
+	t1 = ord(c) - LOWERCASE_OFFSET
+	t2 = ord(k) - LOWERCASE_OFFSET
+	return ALPHABET[(t1 - t2) % len(ALPHABET)]
+
+def b16_decode(enc):
+	res = [ALPHABET.index(c) for c in enc]
+	return (res[0] << 4 | res[1])
+
+def decrypt(encrypted_flag):
+	#for every possible key
+	for alpha in ALPHABET:
+		#for single key unshift the encrypted flag
+		splitted_ecnrypted_flag= ''.join([shift(c, alpha) for c in encrypted_flag])
+		#split the encrypted flag into pairs of two
+		splitted_ecnrypted_flag = [splitted_ecnrypted_flag[i:i+2] for i in range(0, len(encrypted_flag), 2)]
+		#decode the unsifted pairs
+		flag = ''.join([b16_decode(pair) for pair in splitted_ecnrypted_flag])
+		print(' Key: ', alpha, 'with length: ', len(flag), ' Flag: ', flag)
+
+decrypt(encoded_flag)
